8077fd385e66a78095c0e7e8c791549d97ce5279
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-10-31 22:19:20 -0300

    Configuration logic for game

--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
+import { Alert, Platform, StyleSheet, Text, View } from "react-native";
 import { NumberContainer } from "../components/game/NumberContainer";
+import { PrimaryButton } from "../components/ui/PrimaryButton";
 import { Title } from "../components/ui/Title";
 
 function generateRandomBetween(min, max, exclude) {
@@ -13,9 +14,38 @@ function generateRandomBetween(min, max, exclude) {
   }
 }
 
+let minBoundary = 1;
+let maxBoundary = 100;
+
 export function GameScreen({ userNumber }) {
   const initialGuess = generateRandomBetween(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+  function nextGuessHandler(direction) {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "higher" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Don't lie!", "You know that this is wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") {
+      maxBoundary = currentGuess - 1;
+    } else {
+      minBoundary = currentGuess + 1;
+    }
+
+    const newRandomNumber = generateRandomBetween(
+      minBoundary,
+      maxBoundary,
+      currentGuess
+    );
+
+    setCurrentGuess(newRandomNumber);
+  }
 
   return (
     <View style={styles.screen}>
@@ -24,9 +54,16 @@ export function GameScreen({ userNumber }) {
 
       <View>
         <Text>Higher or Lower</Text>
-      </View>
 
-      <View></View>
+        <View>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "lower")}>
+            -
+          </PrimaryButton>
+          <PrimaryButton onPress={nextGuessHandler.bind(this, "higher")}>
+            +
+          </PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
